a24645795de1f67cea8b4db0ed762c82a9e76c55
codebymian <codebymian@gmail.com>
2026-06-14 02:04:12 +0500

    Update customize.sh  Remove Samsung Encryption

--- target/y2q/patches/vendor/customize.sh
+++ target/y2q/patches/vendor/customize.sh
@@ -86,3 +86,12 @@ sed -i '\|/dev/block/bootdevice/by-name/userdata|c\
 /dev/block/bootdevice/by-name/userdata                 /data                  f2fs    noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678,inlinecrypt    latemount,wait,check,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,keydirectory=/metadata/vold/metadata_encryption,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc,quota,reservedsize=128M,checkpoint=fs' \
 "$WORK_DIR/vendor/etc/fstab.qcom"
 LOG_STEP_OUT
+
+LOG_STEP_IN "- Remove Samsung Encryption"
+sed -i -E \
+    's/^([^#].*?)fileencryption=[^,]*(.*)$/# &\n\1encryptable\2/' \
+    "$WORK_DIR/vendor/etc/fstab.qcom"
+sed -i -E \
+    's/^([^#].*?)forceencrypt=[^,]*(.*)$/# &\n\1encryptable\2/' \
+    "$WORK_DIR/vendor/etc/fstab.qcom"
+LOG_STEP_OUT
